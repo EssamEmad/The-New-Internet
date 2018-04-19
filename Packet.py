92a24d7be452482c5ef6387a160bc214ec4ffaa1
Essam Ewaisha <essamemad16@gmail.com>
2018-04-19 08:26:59 +0200

    Added packet class

---
+++ Packet.py
@@ -0,0 +1,29 @@
+from random import *
+class Packet:
+    def __init__(self, length, seqn,data, plp, pcorruption):
+        self.length = length
+        self.seqn = seqn
+        self.data = data
+        self.plp = RandomGenerator(plp)
+        self.pcorruption = RandomGenerator(pcorruption)
+
+    def isACKType(self):
+        return self.length == 8 #Acks are only 8 bytes in length
+
+    def isCorrupt(self):
+        return self.pcorruption.random()
+
+    def isLost(self):
+        return self.plp.random()
+
+
+
+class RandomGenerator:
+
+    def __init__(self, probability = 0):
+        self.p = probability
+
+    def random():
+        """Returns true or false depending on the probability that initialized the object with"
+        """
+        return random.random() < probability
