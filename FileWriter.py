14f0c7ad8ad7d31c1ba859596417de708af67763
tifa64 <krietallo@gmail.com>
2018-05-01 17:48:11 +0200

    Support all types of files

--- FileWriter.py
+++ FileWriter.py
@@ -1,11 +1,11 @@
 class FileWriter:
 
     def __init__(self, fileName):
-        self.file = open('new_' + fileName, 'w')
-        self.data = ''
+        self.file = open('new_' + fileName, 'wb')
+        self.data = b''
     def appendPackets(self, packets):
         for packet in packets:
-            self.data += packet.data.decode("utf-8")
+            self.data += packet.data
         print('Filewriter packets appended: {}'.format(packets))
     def write(self):
         print('Writing pakckets with length:{}'.format(len(self.data)))
