2b64e24b075ae84ae3db05804f053bd03dff1b3d
Essam Ewaisha <essamemad16@gmail.com>
2018-05-06 23:43:43 +0200

    Added echos to the script

--- Verifier.sh
+++ Verifier.sh
@@ -1,3 +1,6 @@
+echo 'Please note that if this script failed, it doesnt necessarily mean that the project'
+echo 'is buggy. It could be that the shell script has failed, but the output was correct'
+echo 'Enjoy =)\n\n'
 FILE_NAME='TestingFile-1984.txt'
 cd Clients
 DIRS=$(find . -maxdepth 1 -mindepth 1 -type d)
@@ -8,4 +11,4 @@ cd $DIR
 cmp $FILE_NAME ../../$FILE_NAME
 cd ../
 done
-echo 'If there are no errors prior to this line, then its working'
+echo 'If there are no errors prior to this line (After <enjoy =)>), then its working'
